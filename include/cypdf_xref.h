58a1ae35436b7a8a2e2a4d851194a14e77a2d5af
Jupieterrr <pieterteb@gmail.com>
2024-09-07 17:32:42 +0200

    Migrated resources to repo.

---
+++ include/cypdf_xref.h
@@ -0,0 +1,15 @@
+#ifndef CYPDF_XREF_H
+#define CYPDF_XREF_H
+
+
+#include <stdio.h>
+
+#include "cypdf_doc.h"
+
+
+
+void CYPDF_Write_Xref(FILE* fp, CYPDF_Doc* pdf);
+
+
+
+#endif /* CYPDF_XREF_H */
