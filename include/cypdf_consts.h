10a5855b66cd14781a94b2d5e48ffbd15ae44cb6
Jupieterrr <pieterteb@gmail.com>
2024-09-08 18:27:27 +0200

    Fixed standardized A series (0-10) dimensions.

--- include/cypdf_consts.h
+++ include/cypdf_consts.h
@@ -2,9 +2,8 @@
 #define CYPDF_CONSTS_H
 
 
-#include <math.h>
-
 #include "cypdf_types.h"
+#include "cypdf_version.h"
 
 
 
@@ -37,45 +36,52 @@
 #define CYPDF_MM_TO_UU(x)           (72 / 25.4 * (x))
 #define CYPDF_UU_TO_MM(x)           (25.4 / 72 * (x))
 
-#define CYPDF_A1_WIDTH              ((CYPDF_REAL)round(CYPDF_MM_TO_UU(594)))
-#define CYPDF_A1_HEIGHT             ((CYPDF_REAL)round(CYPDF_MM_TO_UU(841)))
-#define CYPDF_A1_MEDIABOX           ((CYPDF_Rect){ 0.0, 0.0, (CYPDF_A1_WIDTH), (CYPDF_A1_HEIGHT) })
+#define CYPDF_MAX_PAGE_SIZE         14400           /* In User Units which equals to 200 inches. */
+#define CYPDF_MIN_PAGE_SIZE         3               /* In User Units which equals to 0.04 inches. */
 
-#define CYPDF_A2_WIDTH              ((CYPDF_REAL)round(CYPDF_MM_TO_UU(420)))
-#define CYPDF_A2_HEIGHT             ((CYPDF_REAL)round(CYPDF_MM_TO_UU(594)))
-#define CYPDF_A2_MEDIABOX           ((CYPDF_Rect){ 0.0, 0.0, (CYPDF_A2_WIDTH), (CYPDF_A2_HEIGHT) })
+#define CYPDF_A0_WIDTH              ((CYPDF_REAL)CYPDF_MM_TO_UU(841))
+#define CYPDF_A0_HEIGHT             ((CYPDF_REAL)CYPDF_MM_TO_UU(1189))
+#define CYPDF_A0_MEDIABOX           ((CYPDF_Rect){ 0.0, 0.0, CYPDF_A0_WIDTH, CYPDF_A0_HEIGHT})
 
-#define CYPDF_A3_WIDTH              ((CYPDF_REAL)round(CYPDF_MM_TO_UU(297)))
-#define CYPDF_A3_HEIGHT             ((CYPDF_REAL)round(CYPDF_MM_TO_UU(420)))
-#define CYPDF_A3_MEDIABOX           ((CYPDF_Rect){ 0.0, 0.0, (CYPDF_A3_WIDTH), (CYPDF_A3_HEIGHT) })
+#define CYPDF_A1_WIDTH              ((CYPDF_REAL)CYPDF_MM_TO_UU(594))
+#define CYPDF_A1_HEIGHT             ((CYPDF_REAL)CYPDF_MM_TO_UU(841))
+#define CYPDF_A1_MEDIABOX           ((CYPDF_Rect){ 0.0, 0.0, CYPDF_A1_WIDTH, CYPDF_A1_HEIGHT })
 
-#define CYPDF_A4_WIDTH              ((CYPDF_REAL)round(CYPDF_MM_TO_UU(210)))
-#define CYPDF_A4_HEIGHT             ((CYPDF_REAL)round(CYPDF_MM_TO_UU(297)))
-#define CYPDF_A4_MEDIABOX           ((CYPDF_Rect){ 0.0, 0.0, (CYPDF_A4_WIDTH), (CYPDF_A4_HEIGHT) })
+#define CYPDF_A2_WIDTH              ((CYPDF_REAL)CYPDF_MM_TO_UU(420))
+#define CYPDF_A2_HEIGHT             ((CYPDF_REAL)CYPDF_MM_TO_UU(594))
+#define CYPDF_A2_MEDIABOX           ((CYPDF_Rect){ 0.0, 0.0, CYPDF_A2_WIDTH, CYPDF_A2_HEIGHT })
 
-#define CYPDF_A5_WIDTH              ((CYPDF_REAL)round(CYPDF_MM_TO_UU(148)))
-#define CYPDF_A5_HEIGHT             ((CYPDF_REAL)round(CYPDF_MM_TO_UU(210)))
-#define CYPDF_A5_MEDIABOX           ((CYPDF_Rect){ 0.0, 0.0, (CYPDF_A5_WIDTH), (CYPDF_A5_HEIGHT) })
+#define CYPDF_A3_WIDTH              ((CYPDF_REAL)CYPDF_MM_TO_UU(297))
+#define CYPDF_A3_HEIGHT             ((CYPDF_REAL)CYPDF_MM_TO_UU(420))
+#define CYPDF_A3_MEDIABOX           ((CYPDF_Rect){ 0.0, 0.0, CYPDF_A3_WIDTH, CYPDF_A3_HEIGHT })
 
-#define CYPDF_A6_WIDTH              ((CYPDF_REAL)round(CYPDF_MM_TO_UU(105)))
-#define CYPDF_A6_HEIGHT             ((CYPDF_REAL)round(CYPDF_MM_TO_UU(148)))
-#define CYPDF_A6_MEDIABOX           ((CYPDF_Rect){ 0.0, 0.0, (CYPDF_A6_WIDTH), (CYPDF_A6_HEIGHT) })
+#define CYPDF_A4_WIDTH              ((CYPDF_REAL)CYPDF_MM_TO_UU(210))
+#define CYPDF_A4_HEIGHT             ((CYPDF_REAL)CYPDF_MM_TO_UU(297))
+#define CYPDF_A4_MEDIABOX           ((CYPDF_Rect){ 0.0, 0.0, CYPDF_A4_WIDTH, CYPDF_A4_HEIGHT })
 
-#define CYPDF_A7_WIDTH              ((CYPDF_REAL)round(CYPDF_MM_TO_UU(74)))
-#define CYPDF_A7_HEIGHT             ((CYPDF_REAL)round(CYPDF_MM_TO_UU(105)))
-#define CYPDF_A7_MEDIABOX           ((CYPDF_Rect){ 0.0, 0.0, (CYPDF_A7_WIDTH), (CYPDF_A7_HEIGHT) })
+#define CYPDF_A5_WIDTH              ((CYPDF_REAL)CYPDF_MM_TO_UU(148))
+#define CYPDF_A5_HEIGHT             ((CYPDF_REAL)CYPDF_MM_TO_UU(210))
+#define CYPDF_A5_MEDIABOX           ((CYPDF_Rect){ 0.0, 0.0, CYPDF_A5_WIDTH, CYPDF_A5_HEIGHT })
 
-#define CYPDF_A8_WIDTH              ((CYPDF_REAL)round(CYPDF_MM_TO_UU(52)))
-#define CYPDF_A8_HEIGHT             ((CYPDF_REAL)round(CYPDF_MM_TO_UU(74)))
-#define CYPDF_A8_MEDIABOX           ((CYPDF_Rect){ 0.0, 0.0, (CYPDF_A8_WIDTH), (CYPDF_A8_HEIGHT) })
+#define CYPDF_A6_WIDTH              ((CYPDF_REAL)CYPDF_MM_TO_UU(105))
+#define CYPDF_A6_HEIGHT             ((CYPDF_REAL)CYPDF_MM_TO_UU(148))
+#define CYPDF_A6_MEDIABOX           ((CYPDF_Rect){ 0.0, 0.0, CYPDF_A6_WIDTH, CYPDF_A6_HEIGHT })
 
-#define CYPDF_A9_WIDTH              ((CYPDF_REAL)round(CYPDF_MM_TO_UU(37)))
-#define CYPDF_A9_HEIGHT             ((CYPDF_REAL)round(CYPDF_MM_TO_UU(52)))
-#define CYPDF_A9_MEDIABOX           ((CYPDF_Rect){ 0.0, 0.0, (CYPDF_A9_WIDTH), (CYPDF_A9_HEIGHT) })
+#define CYPDF_A7_WIDTH              ((CYPDF_REAL)CYPDF_MM_TO_UU(74))
+#define CYPDF_A7_HEIGHT             ((CYPDF_REAL)CYPDF_MM_TO_UU(105))
+#define CYPDF_A7_MEDIABOX           ((CYPDF_Rect){ 0.0, 0.0, CYPDF_A7_WIDTH, CYPDF_A7_HEIGHT })
 
-#define CYPDF_A10_WIDTH             ((CYPDF_REAL)round(CYPDF_MM_TO_UU(26)))
-#define CYPDF_A10_HEIGHT            ((CYPDF_REAL)round(CYPDF_MM_TO_UU(37)))
-#define CYPDF_A10_MEDIABOX          ((CYPDF_Rect){ 0.0, 0.0, (CYPDF_A10_WIDTH), (CYPDF_A10_HEIGHT) })
+#define CYPDF_A8_WIDTH              ((CYPDF_REAL)CYPDF_MM_TO_UU(52))
+#define CYPDF_A8_HEIGHT             ((CYPDF_REAL)CYPDF_MM_TO_UU(74))
+#define CYPDF_A8_MEDIABOX           ((CYPDF_Rect){ 0.0, 0.0, CYPDF_A8_WIDTH, CYPDF_A8_HEIGHT })
+
+#define CYPDF_A9_WIDTH              ((CYPDF_REAL)CYPDF_MM_TO_UU(37))
+#define CYPDF_A9_HEIGHT             ((CYPDF_REAL)CYPDF_MM_TO_UU(52))
+#define CYPDF_A9_MEDIABOX           ((CYPDF_Rect){ 0.0, 0.0, CYPDF_A9_WIDTH, CYPDF_A9_HEIGHT })
+
+#define CYPDF_A10_WIDTH             ((CYPDF_REAL)CYPDF_MM_TO_UU(26))
+#define CYPDF_A10_HEIGHT            ((CYPDF_REAL)CYPDF_MM_TO_UU(37))
+#define CYPDF_A10_MEDIABOX          ((CYPDF_Rect){ 0.0, 0.0, CYPDF_A10_WIDTH, CYPDF_A10_HEIGHT })
 
 
 
