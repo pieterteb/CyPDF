b5a7da6ec058e902454ea99e27cf2db9f4768ac5
Jupieterrr <pieterteb@gmail.com>
2024-09-08 22:42:59 +0200

    Paths can now be created and printed.

--- include/cypdf_graphics.h
+++ include/cypdf_graphics.h
@@ -9,21 +9,25 @@
 
 
 /* Path Construction Operator Codes (PCOC) */
-#define CYPDF_PCOC_NEW                  "m"
-#define CYPDF_PCOC_LINESEG              "l"
-#define CYPDF_PCOC_CBEZIER              "c"
-#define CYPDF_PCOC_VBEZIER              "v"
-#define CYPDF_PCOC_YBEZIER              "y"
-#define CYPDF_PCOC_CLOSE                "h"
-#define CYPDF_PCOC_RECT                 "re"
+#define CYPDF_PCOC_NEW                      "m"
+#define CYPDF_PCOC_LINESEG                  "l"
+#define CYPDF_PCOC_CBEZIER                  "c"
+#define CYPDF_PCOC_VBEZIER                  "v"
+#define CYPDF_PCOC_YBEZIER                  "y"
+#define CYPDF_PCOC_CLOSE                    "h"
+#define CYPDF_PCOC_RECT                     "re"
 
 /* Path-painting Operator Codes (PPOC) */
-#define CYPDF_PPOC_STROKE               "S"
-#define CYPDF_PPOC_CLOSE_STROKE         "s"
-#define CYPDF_PPOC_FILL                 "f"
-// #define CYPDF_PPOC_FFILL                "F" /* Should never be used. Only exists for compatibility. */
-#define CYPDF_PPOC_EORFILL              "f*"
-#define CYPDF_PPOC_
+#define CYPDF_PPOC_STROKE                   "S"
+#define CYPDF_PPOC_CLOSE_STROKE             "s"
+#define CYPDF_PPOC_NWNRFILL                 "f"
+// #define CYPDF_PPOC_FFILL                    "F" /* Should never be used. Only exists for compatibility. */
+#define CYPDF_PPOC_EORFILL                  "f*"
+#define CYPDF_PPOC_NWNRFILL_STROKE          "B"
+#define CYPDF_PPOC_EORFILL_STROKE           "B*"
+#define CYPDF_PPOC_CLOSE_NWNRFILL_STROKE    "b"
+#define CYPDF_PPOC_CLOSE_EORFILL_STROKE     "b*"
+#define CYPDF_PPOC_END                      "n"
 
 
 /* Path Construction Operators (PCO) */
@@ -55,14 +59,14 @@ h
 enum CYPDF_PPO_TYPE {
     CYPDF_PPO_STROKE = 0,               /* Stroke the path. */
     CYPDF_PPO_CLOSE_STROKE,             /* Close and stroke the path. Same effect as h S. */
-    CYPDF_PPO_FILL,                     /* */
+    CYPDF_PPO_NWNRFILL,                 /* Fill the path, using the nonzero winding number rule (NWNR, see pdfreference1.7 page 232) to determine the region to fill. */
     // CYPDF_PPO_FFILL,
-    CYPDF_PPO_EORFILL,
-    CYPDF_PPO_FILL_STROKE,
-    CYPDF_PPO_EORFILL_STROKE,
-    CYPDF_PPO_CLOSE_FILL_STROKE,
-    CYPDF_PPO_CLOSE_EORFILL_STROKE,
-    CYPDF_PPO_END,
+    CYPDF_PPO_EORFILL,                  /* Fill the path, using the even-odd rule (EOR, see pdfreference1.7 page 233) to determine the region to fill. */
+    CYPDF_PPO_NWNRFILL_STROKE,          /* Fill and then stroke the path, using the nonzero winding number rule to determine the region to fill. */
+    CYPDF_PPO_EORFILL_STROKE,           /* Fill and then stroke the path, using the even-odd rule to determine the region to fill. */
+    CYPDF_PPO_CLOSE_NWNRFILL_STROKE,    /* Close, fill, and then stroke the path, using the nonzero winding number rule to determine the region to fill. Same effect as h B. */
+    CYPDF_PPO_CLOSE_EORFILL_STROKE,     /* Close, fill, and then stroke the path, using the even-odd rule to determine the region to fill. Same effect as h B*. */
+    CYPDF_PPO_END,                      /* End the path object without filling or stroking it. */
 
     CYPDF_PPO_COUNT,
 
@@ -81,16 +85,18 @@ typedef struct _CYPDF_PCO {
 
 
 typedef struct _CYPDF_Path {
-    CYPDF_PCO**                 pcos;
-    CYPDF_SIZE                  pco_count;
+    CYPDF_PCO**                 pcos;           /* Path construction operators. */
+    CYPDF_SIZE                  pco_count;      /* Number of path construction operators. */
+
+    enum CYPDF_PPO_TYPE         ppo;            /* Path-painting operator. */
 } CYPDF_Path;
 
 
 CYPDF_PCO* CYPDF_PCO_New(enum CYPDF_PCO_TYPE operator, CYPDF_Point endpoint,  CYPDF_Point operand1,  CYPDF_Point operand2);
 
-void CYPDF_Path_Print(FILE* fp, CYPDF_Path* path);
+CYPDF_Path* CYPDF_Path_New(CYPDF_PCO** pcos, CYPDF_SIZE pco_count, enum CYPDF_PPO_TYPE ppo);
 
-CYPDF_Path* CYPDF_Path_New(CYPDF_PCO** pcos, CYPDF_SIZE pco_count);
+void CYPDF_Path_Print(FILE* fp, CYPDF_Path* path);
 
 void CYPDF_Path_Append(CYPDF_Path* path, CYPDF_PCO* pco);
 
--- src/cypdf_graphics.c
+++ src/cypdf_graphics.c
@@ -18,7 +18,7 @@ CYPDF_PCO* CYPDF_PCO_New(enum CYPDF_PCO_TYPE operator, CYPDF_Point endpoint,  CY
     return pco;
 }
 
-CYPDF_Path* CYPDF_Path_New(CYPDF_PCO** pcos, CYPDF_SIZE pco_count) {
+CYPDF_Path* CYPDF_Path_New(CYPDF_PCO** pcos, CYPDF_SIZE pco_count, enum CYPDF_PPO_TYPE ppo) {
     CYPDF_Path* path = CYPDF_smalloc(sizeof(CYPDF_Path));
 
     if (pco_count) {
@@ -27,6 +27,7 @@ CYPDF_Path* CYPDF_Path_New(CYPDF_PCO** pcos, CYPDF_SIZE pco_count) {
         path->pcos = NULL;
     }
     path->pco_count = pco_count;
+    path->ppo = ppo;
 
     return path;
 }
@@ -36,25 +37,25 @@ static void CYPDF_PCO_Print(FILE* fp, CYPDF_PCO* pco) {
         switch (pco->operator)
         {
         case CYPDF_PCO_NEW:
-            fprintf(fp, "%f %f m", pco->endpoint.x, pco->endpoint.y);
+            fprintf(fp, "%f %f " CYPDF_PCOC_NEW, pco->endpoint.x, pco->endpoint.y);
             break;
         case CYPDF_PCO_LINESEG:
-            fprintf(fp, "%f %f l", pco->endpoint.x, pco->endpoint.y);
+            fprintf(fp, "%f %f " CYPDF_PCOC_LINESEG, pco->endpoint.x, pco->endpoint.y);
             break;
         case CYPDF_PCO_CBEZIER:
-            fprintf(fp, "%f %f %f %f %f %f c", pco->operand1.x, pco->operand1.y, pco->operand2.x, pco->operand2.y, pco->endpoint.x, pco->endpoint.y);
+            fprintf(fp, "%f %f %f %f %f %f " CYPDF_PCOC_YBEZIER, pco->operand1.x, pco->operand1.y, pco->operand2.x, pco->operand2.y, pco->endpoint.x, pco->endpoint.y);
             break;
         case CYPDF_PCO_VBEZIER:
-            fprintf(fp, "%f %f %f %f v", pco->operand2.x, pco->operand2.y, pco->endpoint.x, pco->endpoint.y);
+            fprintf(fp, "%f %f %f %f " CYPDF_PCOC_VBEZIER, pco->operand2.x, pco->operand2.y, pco->endpoint.x, pco->endpoint.y);
             break;
         case CYPDF_PCO_YBEZIER:
-            fprintf(fp, "%f %f %f %f y", pco->operand1.x, pco->operand1.y, pco->endpoint.x, pco->endpoint.y);
+            fprintf(fp, "%f %f %f %f " CYPDF_PCOC_YBEZIER, pco->operand1.x, pco->operand1.y, pco->endpoint.x, pco->endpoint.y);
             break;
         case CYPDF_PCO_CLOSE:
-            fputc('h', fp);
+            fprintf(fp, CYPDF_PCOC_CLOSE);
             break;
         case CYPDF_PCO_RECT:
-            fprintf(fp, "%f %f %f %f re", pco->endpoint.x, pco->endpoint.y, pco->operand1.x, pco->operand1.y);
+            fprintf(fp, "%f %f %f %f " CYPDF_PCOC_RECT, pco->endpoint.x, pco->endpoint.y, pco->operand1.x, pco->operand1.y);
             break;
         default:
             break;
@@ -68,6 +69,40 @@ void CYPDF_Path_Print(FILE* fp, CYPDF_Path* path) {
             CYPDF_PCO_Print(fp, path->pcos[i]);
             CYPDF_Write_NL(fp);
         }
+
+        switch (path->ppo)
+        {
+        case CYPDF_PPO_STROKE:
+            fprintf(fp, CYPDF_PPOC_STROKE);
+            break;
+        case CYPDF_PPO_CLOSE_STROKE:
+            fprintf(fp, CYPDF_PPOC_CLOSE_STROKE);
+            break;
+        case CYPDF_PPO_NWNRFILL:
+            fprintf(fp, CYPDF_PPOC_NWNRFILL);
+            break;
+        case CYPDF_PPO_EORFILL:
+            fprintf(fp, CYPDF_PPOC_EORFILL);
+            break;
+        case CYPDF_PPO_NWNRFILL_STROKE:
+            fprintf(fp, CYPDF_PPOC_NWNRFILL_STROKE);
+            break;
+        case CYPDF_PPO_EORFILL_STROKE:
+            fprintf(fp, CYPDF_PPOC_EORFILL_STROKE);
+            break;
+        case CYPDF_PPO_CLOSE_NWNRFILL_STROKE:
+            fprintf(fp, CYPDF_PPOC_CLOSE_NWNRFILL_STROKE);
+            break;
+        case CYPDF_PPO_CLOSE_EORFILL_STROKE:
+            fprintf(fp, CYPDF_PPOC_CLOSE_EORFILL_STROKE);
+            break;
+        case CYPDF_PPO_END:
+            fprintf(fp, CYPDF_PPOC_END);
+            break;
+        default:
+            break;
+        }
+        CYPDF_Write_NL(fp);
     }
 }
 
